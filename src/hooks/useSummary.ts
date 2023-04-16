d621bf4d9e7f75a8b9fa6f13893adcb4cd04d2df
jilherme <rosa.guilhermediogo@gmail.com>
2023-04-15 22:17:06 -0300

    add: useMemo to summary hook

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,3 +1,4 @@
+import { useMemo } from 'react'
 import { useContextSelector } from 'use-context-selector'
 
 import { TransactionsContext } from '../contexts/TransactionsContext'
@@ -10,24 +11,27 @@ export function useSumary() {
   // reduce permite percorrer um array e reduzi-lo a alguma nova estrutura de dados
   // neste caso, converter o array de transactions num objeto:
 
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === 'income') {
-        acc.income += transaction.price
-        acc.total += transaction.price
-      } else {
-        acc.outcome += transaction.price
-        acc.total -= transaction.price
-      }
+  // a variavel summary só vai ser recriada quando o array de transactions for alterado
+  const summary = useMemo(() => {
+    return transactions.reduce(
+      (acc, transaction) => {
+        if (transaction.type === 'income') {
+          acc.income += transaction.price
+          acc.total += transaction.price
+        } else {
+          acc.outcome += transaction.price
+          acc.total -= transaction.price
+        }
 
-      return acc
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
-  )
+        return acc
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    )
+  }, [transactions])
 
   return summary
 }
